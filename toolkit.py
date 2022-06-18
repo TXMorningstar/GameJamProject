f8225b2519accc21cb0f103f41c634bca9511f06
Faust <2565158031@qq.com>
2022-06-18 15:00:51 +0800

    Create toolkit.py

---
+++ toolkit.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+# 游戏路径相关 ##############################################
+gameFolder = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+
+def res_path(relative_path):
+    return "%s\%s" % (gameFolder, relative_path)
+
+
+# 定义一些常用颜色 ##########################################
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
