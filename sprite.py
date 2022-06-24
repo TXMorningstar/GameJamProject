dd51b28f8c5a2b4ff1c7867e4aed320884fba91d
McPlus <97387028+McPlusss@users.noreply.github.com>
2022-06-25 01:46:52 +0800

    fix: 修复sprite.py的错误

--- sprite.py
+++ sprite.py
@@ -28,9 +28,9 @@ class CapitalCard(pygame.sprite.Sprite):
         self.card_back_big = pygame.image.load(tk.res_path("image/capital_card_back_big.png"))
 
         self.rect = self.card_front.get_rect()
-        self.rect.midTop = position
+        self.rect.midtop = position
         self.rect_big = self.card_front_big.get_rect()
-        self.rect_big.midBottom = position
+        self.rect_big.midbottom = position
 
     def update(self):
         pass
@@ -57,6 +57,6 @@ class Bureaucrat(pygame.sprite.Sprite):
         self.card_back_big = pygame.image.load(tk.res_path("image/bureaucrat_card_back_big.png"))
 
         self.rect = self.card_front.get_rect()
-        self.rect.midTop = position
+        self.rect.midtop = position
         self.rect_big = self.card_front_big.get_rect()
-        self.rect_big.midBottom = position
+        self.rect_big.midbottom = position
