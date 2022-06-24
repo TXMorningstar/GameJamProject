dcfb179e988474aa7ac3b0c41a7f09276712085e
Faust <2565158031@qq.com>
2022-06-25 01:37:18 +0800

    <comment>简单的注释

--- sprite.py
+++ sprite.py
@@ -3,6 +3,7 @@ import toolkit as tk
 
 
 class Board(pygame.sprite.Sprite):
+    """背景的精灵"""
     def __init__(self, main_board, position: tuple) -> None:
         pygame.sprite.Sprite.__init__(self)
         self.main_board = pygame.image.load(tk.res_path(main_board))
@@ -17,8 +18,10 @@ class Board(pygame.sprite.Sprite):
 
 
 class CapitalCard(pygame.sprite.Sprite):
+    """资本家的卡牌"""
     def __init__(self, illustration, illustration_big, position: tuple) -> None:
         pygame.sprite.Sprite.__init__(self)
+        # 创建图片的surface对象，illustration是卡牌上的插画，front是卡牌正面，back是卡背，big后缀是大号的卡牌
         self.illustration = pygame.image.load(tk.res_path(illustration))
         self.illustration_big = pygame.image.load(tk.res_path(illustration_big))
         self.card_front = pygame.image.load(tk.res_path("image/capital_card_front.png"))
@@ -32,10 +35,11 @@ class CapitalCard(pygame.sprite.Sprite):
         self.rect_big.midbottom = position
 
     def update(self):
+        """用于group的调用"""
         pass
 
     def draw(self, screen, big=False):
-        """绘制的时候先绘制插画再绘制"""
+        """绘制的时候先绘制插画再绘制牌框，big参数为True时绘制大的卡牌，否则绘制小号的"""
         if not big:
             screen.blit(self.illustration, self.rect)
             screen.blit(self.card_front, self.rect)
@@ -45,8 +49,10 @@ class CapitalCard(pygame.sprite.Sprite):
 
 
 class Bureaucrat(pygame.sprite.Sprite):
+    """官僚的卡牌"""
     def __init__(self, illustration, illustration_big, position) -> None:
         pygame.sprite.Sprite.__init__(self)
+        # 创建图片的surface对象，illustration是卡牌上的插画，front是卡牌正面，back是卡背，big后缀是大号的卡牌
         self.illustration = pygame.image.load(tk.res_path(illustration))
         self.illustration_big = pygame.image.load(tk.res_path(illustration_big))
         self.card_front = pygame.image.load(tk.res_path("image/bureaucrat_card_front.png"))
@@ -58,3 +64,16 @@ class Bureaucrat(pygame.sprite.Sprite):
         self.rect.midtop = position
         self.rect_big = self.card_front_big.get_rect()
         self.rect_big.midbottom = position
+
+    def update(self):
+        """用于group的调用"""
+        pass
+
+    def draw(self, screen, big=False):
+        """绘制的时候先绘制插画再绘制牌框，big参数为True时绘制大的卡牌，否则绘制小号的"""
+        if not big:
+            screen.blit(self.illustration, self.rect)
+            screen.blit(self.card_front, self.rect)
+        else:
+            screen.blit(self.illustration_big, self.rect.midtop)
+            screen.blit(self.card_front_big, self.rect.midtop)
